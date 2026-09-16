cf9c279ced6eeb5e6633fe40fda12da2bc9ee8dd
vikashkrtiwari <xyztiwari620@gmail.com>
2026-09-16 17:19:53 +0530

    Add new DSA practice programs

---
+++ abcde2pattern.cpp
@@ -0,0 +1,16 @@
+#include<iostream>  
+using namespace std;
+int main()
+{
+    int row;
+    char col;
+    for(row=1; row<=5; row=row+1)
+    {
+        // char name='a'+row-1;
+        for( char col='a'; col<='e'; col=col+1)
+        {
+            cout<<col<<" ";
+        }
+        cout<<endl;
+    }
+}
