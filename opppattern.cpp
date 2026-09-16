be52a1d98e739c565429727f6e960c5020c76af7
vikashkrtiwari <xyztiwari620@gmail.com>
2026-09-16 15:33:50 +0530

    Add new DSA practice programs

---
+++ opppattern.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int row,col;
+    for(row=5; row>=1; row=row-1)
+    {
+        for(col=5; col>=1; col=col-1){
+            cout<<col<<" ";
+        }
+        cout<<endl;
+    }
+}
---
+++ starpattern.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int j,i;
+    for(j=1; j<=10;j=j+1)
+    {
+        for(i=1; i<=10; i=i+1){
+            cout<<"*"<<"  ";
+        }
+          cout<<endl;
+    }
+  
+}
